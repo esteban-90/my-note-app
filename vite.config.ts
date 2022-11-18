c68c6818075a529bbaa5332c956e4c452cf808dd
Esteban Vera Mantero <estebanvm1990@gmail.com>
2022-11-18 12:10:58 -0300

    rest configs

---
+++ vite.config.ts
@@ -0,0 +1,59 @@
+/// <reference types="vitest"/>
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Note App',
+        short_name: 'Note App',
+        theme_color: '#f68657',
+        background_color: '#1f2124',
+        description: 'Note keeping application made in React and Vite',
+        display: 'fullscreen',
+        orientation: 'any',
+        start_url: '/',
+        icons: [
+          {
+            src: '/assets/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/assets/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/assets/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/assets/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    reporters: 'verbose',
+    setupFiles: './src/tests/setup',
+  },
+})
